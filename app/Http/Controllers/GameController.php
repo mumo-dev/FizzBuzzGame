d1dd66813c6619a5c346842827f4f33dea083210
mumo-dev <samumo.dev@gmail.com>
2019-03-24 18:08:43 +0300

    Fizz Buzz Game

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Status;
+
+class GameController extends Controller
+{
+    public function start()
+    {
+        //create a unique id;;
+        $gameId = uniqid();
+
+        //create current game status and store it in db
+        $status = new Status();
+        $status->gameId = $gameId;
+        $status->save();
+
+        $gameUrl = 'http://'.$_SERVER['HTTP_HOST'] . '/api/game/play/'.$gameId.'?answer=';
+
+        $instructions = " Use the url generated below to play the game.The game usually starts counting at 1 and ends at 100. Append your answer at the end of your generated game_url eg. ". $gameUrl . "1 assuming your response is 1";
+
+        return response()->json([
+            'message'=>'Game started.',
+            'instructions'=>$instructions,
+            'game_url'=>$gameUrl
+        ], 200);
+    }
+
+    public function play(Request $request,$gameId)
+    {
+        $gameStatus = Status::where('gameId', $gameId)->first();
+
+        $startNewGameUrl ='http://'.$_SERVER['HTTP_HOST'] . '/api/game/start';
+
+        //if the gameStatus is null, the gameId is invalid, return
+        if($gameStatus == null){
+            return response()->json([
+                'message'=>'The game session is invalid. Use the url below to start a new game session',
+                'url'=> $startNewGameUrl
+            ]);
+        }
+
+        $answer = $request->answer;
+        $gameUrl = 'http://'.$_SERVER['HTTP_HOST'] . '/api/game/play/'.$gameStatus->gameId.'?answer=';
+       
+        
+        //retrieve previousNumber from database 
+        $previousNumber = $gameStatus->previousNumber;
+
+        $correctResponse = $this->calculateExpectedResponse($previousNumber);
+
+       
+        if($answer == $correctResponse ){
+            //succeeded.
+            //if previousNumber++ == 100 -->end game -->delete the game status from db value from db
+            if($previousNumber++ == 100){
+                $gameStatus->delete();
+                return response()->json([
+                    'message'=>'Congrats. Game Finished. You have won. Use the start game url below to start a new game',
+                    'new_game_url'=> $startNewGameUrl
+                ],200);
+            }
+            else {// the user can continue with the game
+              //update new value in db
+                $gameStatus->previousNumber+=1;
+                $gameStatus->save();
+
+                return response()->json([
+                    'message'=>'Correct answer. Continue the game by entering the next response in game url',
+                    'game_url'=>$gameUrl
+                ],200);
+            }
+        }else {
+            //failed game over --> delete  the game status from db
+            $gameStatus->delete();
+            return response()->json([
+                'message'=>'Whoops! ;( . Sorry, you entered the wrong response. Game Over. Use the url below to start a new game',
+                'new_game_url'=>$startNewGameUrl
+            ],200);
+        }
+
+    }
+
+    private function calculateExpectedResponse($prevNumber)
+    {
+
+        $currentNumber = $prevNumber + 1;
+
+        
+        if($currentNumber % 3 == 0 && $currentNumber % 5 == 0){
+            return "fizzbuzz";
+        }else if ($currentNumber % 3 == 0){
+            return "fizz";
+        }else if ($currentNumber % 5 == 0){
+            return "buzz";
+        }else {
+            return $currentNumber;
+        }
+        
+    }
+}
---
+++ database/migrations/2019_03_24_133851_create_statuses_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('gameId')->unique();
+            $table->unsignedInteger('previousNumber')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('statuses');
+    }
+}
